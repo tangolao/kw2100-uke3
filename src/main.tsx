31f925b48df6d263a22117be3f2dd8f19b059469
thanongsaklaoketkich <thanong_777@hotmail.com>
2026-01-19 15:03:13 +0100

    Start the Map

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React, {useEffect, useRef} from "react";
+import { createRoot } from "react-dom/client";
+import {Map} from "ol";
+import "./application.css";
+
+
+const map = new Map({
+
+})
+function Application() {
+    const  mapRef = useRef();
+    useEffect(() => {
+         mapRef.current.innerText = "Hei, fra JaScripts " + (6*7)
+        map.setTarget(mapRef.current);
+    }, []);
+    return <>
+        <h1>Hello Map Application</h1>
+        <div ref={mapRef}>Here I want the map</div>
+    </>
+
+}
+
+createRoot(document.getElementById("root")!).render(<Application/>);
