7d33386e44a85411db8681990fc2037249eb6ee4
thanongsaklaoketkich <thanong_777@hotmail.com>
2026-01-19 15:20:14 +0100

    Added useGeographic

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React, {useEffect, useRef} from "react";
 import { createRoot } from "react-dom/client";
-import {Map} from "ol";
+
 import "./application.css";
 
+import {Map, View} from "ol";
+import TileLayer from "ol/layer/Tile";
+import {OSM} from "ol/source";
+import {useGeographic} from "ol/proj";
+
+useGeographic();
 
 const map = new Map({
-
+    layers:[new TileLayer({source: new OSM()})],
+    view: new View({center: [10, 59], zoom: 7})
 })
 function Application() {
     const  mapRef = useRef();
